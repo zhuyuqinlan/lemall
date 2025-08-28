d7c8bd95f65dae38446df92921bbb002929ae6c3
zhuyuqin <echonanguo@qq.com>
2025-08-28 17:41:59 +0800

    2025828:完成营销模块全部接口

--- lemall-server/lemall-business/lemall-module-admin/src/main/java/org/nanguo/lemall/business/admin/sale/service/impl/SmsHomeNewProductServiceImpl.java
+++ lemall-server/lemall-business/lemall-module-admin/src/main/java/org/nanguo/lemall/business/admin/sale/service/impl/SmsHomeNewProductServiceImpl.java
@@ -1,13 +1,17 @@
 package org.nanguo.lemall.business.admin.sale.service.impl;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.nanguo.lemall.business.admin.sale.dto.request.SmsHomeNewProductRequestDTO;
 import org.nanguo.lemall.business.admin.sale.dto.response.SmsHomeNewProductResponseDTO;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.nanguo.lemall.common.entity.SmsHomeNewProduct;
 import org.nanguo.lemall.business.admin.sale.mapper.SmsHomeNewProductMapper;
 import org.nanguo.lemall.business.admin.sale.service.SmsHomeNewProductService;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -16,26 +20,47 @@ public class SmsHomeNewProductServiceImpl extends ServiceImpl<SmsHomeNewProductM
 
     @Override
     public boolean create(List<SmsHomeNewProductRequestDTO> homeBrandList) {
-        return false;
+        List<SmsHomeNewProduct> smsHomeNewProducts = homeBrandList.stream().map(e -> {
+            SmsHomeNewProduct smsHomeNewProduct = new SmsHomeNewProduct();
+            BeanUtils.copyProperties(e, smsHomeNewProduct);
+            smsHomeNewProduct.setRecommendStatus(1);
+            smsHomeNewProduct.setSort(0);
+            return smsHomeNewProduct;
+        }).toList();
+        return super.saveBatch(smsHomeNewProducts);
     }
 
     @Override
     public boolean updateSort(Long id, Integer sort) {
-        return false;
+        return super.update(Wrappers.<SmsHomeNewProduct>lambdaUpdate()
+                .eq(SmsHomeNewProduct::getId, id)
+                .set(SmsHomeNewProduct::getSort, sort)
+        );
     }
 
     @Override
     public boolean delete(List<Long> ids) {
-        return false;
+        return super.removeByIds(ids);
     }
 
     @Override
     public boolean updateRecommendStatus(List<Long> ids, Integer recommendStatus) {
-        return false;
+        return super.update(Wrappers.<SmsHomeNewProduct>lambdaUpdate()
+                .in(SmsHomeNewProduct::getId, ids)
+                .set(SmsHomeNewProduct::getRecommendStatus, recommendStatus)
+        );
     }
 
     @Override
     public IPage<SmsHomeNewProductResponseDTO> listPage(String productName, Integer recommendStatus, Integer pageSize, Integer pageNum) {
-        return null;
+        return super.page(new Page<>(pageNum,pageSize),Wrappers.<SmsHomeNewProduct>lambdaQuery()
+                .like(StringUtils.hasText(productName),SmsHomeNewProduct::getProductName,productName)
+                .eq(recommendStatus != null,SmsHomeNewProduct::getRecommendStatus,recommendStatus)
+                .orderByDesc(SmsHomeNewProduct::getSort)
+        ).convert(e -> {
+            SmsHomeNewProductResponseDTO responseDTO = new SmsHomeNewProductResponseDTO();
+            BeanUtils.copyProperties(e,responseDTO);
+            return responseDTO;
+        });
     }
 }
